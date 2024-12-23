8064661ea01201cb3bf3c11f6b11ca0c6ca05864
AMKH <alimahmoodikharil@proton.me>
2024-12-23 12:32:05 +0330

    Implemented part of CRUD

---
+++ todo/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from .views import ToDoListView, ToDoDeatilView
+
+
+urlpatterns = [
+    path('todo_list/', ToDoListView.as_view()),
+    path('todo_detail/<int:pk>/', ToDoDeatilView.as_view()),
+]
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .models import ToDo
+from .serializers import ToDoSerializer
+
+
+class ToDoListView(APIView):
+    def get(self, request):
+        items = ToDo.objects.all()
+        serializer = ToDoSerializer(items, many=True)
+        return Response(serializer.data)
+    
+
+    def post(self, request):
+        serializer = ToDoSerializer(data= request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status= status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class ToDoDeatilView(APIView):
+    def get(self,request, pk):
+        item = ToDo.objects.get(id=pk)
+        serializer = ToDoSerializer(item)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+    def delete(self, request, pk):
+        item = ToDo.objects.get(id=pk)
+        item.delete
+
+        return Response({"message":"item deleted successfully"}, status= status.HTTP_204_NO_CONTENT)
