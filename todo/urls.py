ce04ea6b936cf771f995c1a62b86d730d659b11b
AMKH <alimahmoodikharil@proton.me>
2024-12-23 14:35:03 +0330

    Final part

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 
+
 from .views import ToDoListView, ToDoDeatilView
+
+
 
 
 urlpatterns = [
--- todo/views.py
+++ todo/views.py
@@ -1,17 +1,34 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+
 
 from .models import ToDo
 from .serializers import ToDoSerializer
 
 
 class ToDoListView(APIView):
+    @swagger_auto_schema(
+            operation_description='retrieve all todos',
+            responses={200: ToDoSerializer(many=True)}
+    )
+
     def get(self, request):
         items = ToDo.objects.all()
         serializer = ToDoSerializer(items, many=True)
         return Response(serializer.data)
     
+    @swagger_auto_schema(
+            operation_description='Create new todo',
+            request_body=ToDoSerializer,
+            responses= {
+                201: openapi.Response("Created", ToDoSerializer),
+                400: "Bad request"
+            }
+    )
+
 
     def post(self, request):
         serializer = ToDoSerializer(data= request.data)
@@ -23,10 +40,32 @@ class ToDoListView(APIView):
     
 
 class ToDoDeatilView(APIView):
+    @swagger_auto_schema(
+        operation_description="Show the detail of one specific TODO",
+        manual_parameters=[
+            openapi.Parameter(
+            "pk", openapi.IN_PATH, description="PK of TODO", type=openapi.TYPE_INTEGER
+            )
+        ],
+        responses={
+             200: ToDoSerializer,
+             404: "No content(Not Found)"
+        }
+    )
+
+
     def get(self,request, pk):
         item = ToDo.objects.get(id=pk)
         serializer = ToDoSerializer(item)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @swagger_auto_schema(
+        operation_description="delete a ToDo",
+        responses={
+            204: "No Content",
+            404: "Not Found",
+        }
+    )
 
 
     def delete(self, request, pk):
